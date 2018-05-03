f87565616966048d70c2b9d80f15523d0395a4ef
unknown <Bapg66@hotmail.fr>
2018-05-03 20:23:08 +0200

    initial

---
+++ dominion/card/VictoryCard.java
@@ -0,0 +1,14 @@
+package dominion.card;
+import java.util.*;
+import dominion.*;
+
+/**
+ * Les cartes Victoire
+ */
+public abstract class VictoryCard extends Card {
+
+	public VictoryCard(String name, int cost) {
+		super(name, cost);
+		
+	}
+}
